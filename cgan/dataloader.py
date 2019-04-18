505267dcccd9bf95c94b6f2e0c4e561e5f80f00d
ivokun <salahuddin.mi@gmail.com>
2019-04-18 14:31:04 +0900

    Classifier training baseline

---
+++ cgan/dataloader.py
@@ -0,0 +1,168 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from scipy.io import arff
+import pandas as pd
+
+# class DatasetCustom(Dataset):
+#     def __init__(self, data_file, format):
+#         if format == 'csv':
+#             filename = dataset + '.csv'
+#             data = pd.read_csv('datasets/' + filename)
+#         else:
+#             filename = dataset + '.numeric.' + format
+#             data= arff.loadarff('datasets/' + filename)
+#             data = pd.DataFrame(data[0])
+#     def __len__(self):
+#         return len(self.dataframe)
+    
+#     def __getitem__(self, idx):
+#         features
+
+def dataloader(dataset, input_size, batch_size, split='train'):
+    transform = transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
+    if dataset == 'mnist':
+        data_loader = DataLoader(
+            datasets.MNIST('data/mnist', train=True, download=True, transform=transform),
+            batch_size=batch_size, shuffle=True)
+    elif dataset == 'fashion-mnist':
+        data_loader = DataLoader(
+            datasets.FashionMNIST('data/fashion-mnist', train=True, download=True, transform=transform),
+            batch_size=batch_size, shuffle=True)
+    elif dataset == 'cifar10':
+        data_loader = DataLoader(
+            datasets.CIFAR10('data/cifar10', train=True, download=True, transform=transform),
+            batch_size=batch_size, shuffle=True)
+    elif dataset == 'svhn':
+        data_loader = DataLoader(
+            datasets.SVHN('data/svhn', split=split, download=True, transform=transform),
+            batch_size=batch_size, shuffle=True)
+    elif dataset == 'stl10':
+        data_loader = DataLoader(
+            datasets.STL10('data/stl10', split=split, download=True, transform=transform),
+            batch_size=batch_size, shuffle=True)
+    elif dataset == 'lsun-bed':
+        data_loader = DataLoader(
+            datasets.LSUN('data/lsun', classes=['bedroom_train'], transform=transform),
+            batch_size=batch_size, shuffle=True)
+
+    return data_loader
+
+def samplingloader(dataset, format = 'arff'):
+
+    if format == 'csv':
+        filename = dataset + '.csv'
+        data = pd.read_csv('datasets/' + filename)
+    else:
+        filename = dataset + '.numeric.' + format
+        data= arff.loadarff('datasets/' + filename)
+        data = pd.DataFrame(data[0])
+        # data = pd.replace()
+    
+    if dataset == 'vote':
+        data = data.replace(b'republican', 0)
+        data = data.replace(b'democrat', 1)
+        data = data.replace('Class', 'class')
+
+    elif dataset == 'sonar':
+        data = data.replace(b'Rock', 0)
+        data = data.replace(b'Mine', 1)    
+
+    elif dataset == 'sick':
+        data = data.replace(b'negative', 0)
+        data = data.replace(b'sick', 1)
+    
+    elif dataset == 'mushroom':
+        data = data.replace(b'e', 0)
+        data = data.replace(b'p', 1)
+    
+    elif dataset == 'labor':
+        data = data.replace(b'bad', 0)
+        data = data.replace(b'good', 1)
+    
+    elif dataset == 'kr-vs-kp':
+        data = data.replace(b'won', 0)
+        data = data.replace(b'nowin', 1)
+    
+    elif dataset == 'ionosphere':
+        data = data.replace(b'b', 0)
+        data = data.replace(b'g', 1)
+    
+    elif dataset == 'hepatitis':
+        data = data.replace(b'DIE', 0)
+        data = data.replace(b'LIVE', 1)
+    
+    elif dataset == 'heart-statlog':
+        data = data.replace(b'absent', 0)
+        data = data.replace(b'present', 1)
+
+    elif dataset == 'diabetes':
+        data = data.replace(b'tested_negative', 0)
+        data = data.replace(b'tested_positive', 1)
+
+    elif dataset == 'credit-g':
+        data = data.replace(b'good', 0)
+        data = data.replace(b'bad', 1)
+
+    elif dataset == 'credit-a':
+        data = data.replace(b'+', 0)
+        data = data.replace(b'-', 1)
+
+    elif dataset == 'colic':
+        data = data.replace(b'yes', 0)
+        data = data.replace(b'no', 1)
+
+    elif dataset == 'breast-w':
+        data = data.replace(b'benign', 0)
+        data = data.replace(b'malignant', 1)
+    
+    elif dataset == 'breast-cancer':
+        data = data.replace(b'no-recurrence-events', 0)
+        data = data.replace(b'recurrence-events', 1)
+    return data
+
+def split_data(data):
+    ##
+    #   Split test data equally 50% of classes
+    #
+    ##
+    data = data.groupby(['class'])
+    a = data.get_group(0)
+    b = data.get_group(1)
+    a_range = a.shape[0]
+    b_range = b.shape[0]
+    range_data = min(a_range, b_range) // 2
+    train_data_a = a.iloc[range_data:, :]
+    train_data_b = b.iloc[range_data:, :]
+    test_data_a = a.iloc[:range_data,:]
+    test_data_b = b.iloc[:range_data,:]
+    train_data = pd.concat([train_data_a, train_data_b])
+    test_data = pd.concat([test_data_a, test_data_b])
+
+    all_train_data = train_data.sample(frac=1)
+    all_test_data = test_data.sample(frac=1)
+
+    train_data = []
+    test_data = []
+
+    for idx in range(all_train_data.shape[0]):
+        features = all_train_data.iloc[idx, :-1].values.astype(dtype='float64')
+        target = all_train_data.iloc[idx, -1:].values.astype(dtype='float64')
+        data = {'features': torch.from_numpy(features),
+                 'target': target}
+        train_data.append(data)
+    
+    for idx in range(all_test_data.shape[0]):
+        features = all_test_data.iloc[idx, :-1].values.astype(dtype='float64')
+        target = all_test_data.iloc[idx, -1:].values.astype(dtype='float64')
+        data = {'features': torch.from_numpy(features),
+                 'target': target}
+        test_data.append(data)
+
+    # train_data = train_data.iloc[:, 0:train_data.shape[1]-1]
+    # test_data = test_data.iloc[:, 0:test_data.shape[1]-1]
+
+    train_data = DataLoader(train_data, batch_size=4, shuffle=True)
+    test_data = DataLoader(test_data, batch_size=4, shuffle=True)
+    features_num = all_train_data.shape[1]-1
+    return train_data, test_data, features_num
---
+++ cgan/simple_classifier.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+
+from dataloader import samplingloader
+from dataloader import split_data
+
+class Net(nn.Module):
+  
+  def __init__(self, input):
+    super(Net, self).__init__()
+    self.input = input
+
+    self.net = nn.Sequential(
+      nn.Linear(self.input, self.input),
+      nn.ReLU(),
+      nn.Dropout(p=0.2),
+      nn.Linear(self.input,30),
+      nn.ReLU(),
+      nn.Dropout(p=0.2),
+      nn.Linear(30, 1),
+      nn.Sigmoid()
+    )
+    
+  def forward(self, x):
+    return self.net(x)
+
+
+def train(net, train_loader, optimizer, epoch):
+    """Create the training loop"""
+    net.train()
+    criterion = nn.BCELoss()
+    running_loss = 0.0
+    for batch_index, data in enumerate(train_loader):
+        features = data['features']
+        target = data['target']
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = net(features)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if batch_index % 6 == 5:  # print every 6 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch, batch_index + 1, running_loss / 6))
+            running_loss = 0.0
+
+def test(net, test_loader):
+    """Test the DNN"""
+    net.eval()
+    criterion = nn.BCELoss()  # https://pytorch.org/docs/stable/nn.html#bceloss
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for i, data in enumerate(test_loader, 0):
+            features = data['features']
+            target = data['target']
+            output = net(features)
+            # Binarize the output
+            pred = output.apply_(lambda x: 0.0 if x < 0.5 else 1.0)
+            test_loss += criterion(output, target)  # sum up batch loss
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set:\n\tAverage loss: {:.4f}'.format(test_loss))
+    print('\tAccuracy: {}/{} ({:.0f}%)\n'.format(
+            correct,
+            (len(test_loader) * test_loader.batch_size),
+            100. * correct / (len(test_loader) * test_loader.batch_size)))
+
+dataset = 'credit-a'
+data = samplingloader(dataset, 'arff')
+train_data, test_data, features_num = split_data(data)
+torch.manual_seed(42)
+net = Net(features_num).double()
+learning_rate = 0.01
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.5, nesterov=False)
+epochs = 10
+
+print("Training Start")
+for epoch in range(1, epochs+1):
+  train(net, train_data, optimizer, epoch)
+  test(net, test_data)
+
+torch.save(net.state_dict(), 'models/'+dataset+'_without_sampling')
+
+print("DONE")
